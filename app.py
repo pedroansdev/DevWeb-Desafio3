4617cc90a32b35ab35f8a175e3028e9abdf83325
Pedro Augusto <126725401+pedroansdev@users.noreply.github.com>
2023-04-25 08:23:17 -0300

    Adicionando rota POST sql

--- app.py
+++ app.py
@@ -13,10 +13,25 @@ mysql = MySQL(app)
 def index():
     return render_template("index.html")
 
-@app.route("/contato")
-def contato():
-    return render_template("contato.html")
-
 @app.route("/quemsomos")
 def quemSomos():
     return render_template("quemsomos.html")
+
+@app.route("/contato", methods['POST','GET'])
+def contato():
+    if request.method == 'POST':
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contato(email, assunto, descricao) VALUES(%s, %s, %s)", (email, assunto, descricao))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        return 'Envio bem sucedido! \nObrigado pelo seu contato :)'
+
+    return render_template("contato.html")
+
