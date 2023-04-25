7c2b5a74796b726bf53dafb09964c4ee50a6240d
Pedro Augusto <126725401+pedroansdev@users.noreply.github.com>
2023-04-25 08:03:39 -0300

    Adicionando dependencias e conexao sql

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask("__name__", static_folder="./static", template_folder="./templates")
 
+app.config['MYSQL_Host'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'Unes'
+
+mysql = MySQL(app)
+
 @app.route("/")
 def index():
     return render_template("index.html")
